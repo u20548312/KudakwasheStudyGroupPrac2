c872488c77e9cc652be4824485756d31ee7d2627
u20548312 <u20548312@tuks.co.za>
2023-04-04 13:10:49 +0200

    new

--- KudakwasheStudyGroupPrac2/Controllers/StudyGroupController.cs
+++ KudakwasheStudyGroupPrac2/Controllers/StudyGroupController.cs
@@ -13,11 +13,36 @@ namespace KudakwasheStudyGroupPrac2.Controllers
         public ActionResult StudyGroup()
         {
             List<StudyGroupModel> students = new List<StudyGroupModel>();
-            students.Add(new StudyGroupModel { StudentNo = 20548312, Name = "Kudakwashe", Surname = "Mhuriro", EmailAddress = "u20548312@tuks.co.za", myLink = "~/HTML/Kudakwashe.html" });
-            students.Add(new StudyGroupModel { StudentNo = 18242937, Name = "Hector", Surname = "Nhlabathi", EmailAddress = "u18242937@tuks.co.za", myLink = "~/HTML/Hector.html" });
-            students.Add(new StudyGroupModel { StudentNo = 19227206, Name = "Raphael", Surname = "Alphonso", EmailAddress = "u19227206@tuks.co.za", myLink = "~/HTML/Raphael.html" });
-            students.Add(new StudyGroupModel { StudentNo = 20509482, Name = "Claudius", Surname = "Phalane", EmailAddress = "u20509482@tuks.co.za", myLink = "~/HTML/Claudius.html" });
-            students.Add(new StudyGroupModel { StudentNo = 20554232, Name = "Kgaugelo", Surname = "Mosetlha", EmailAddress = "u20554232@tuks.co.za", myLink = "~/HTML/Kgaugelo.html" });
+            students.Add(new StudyGroupModel { StudentNo = 20548312,
+                Name = "Kudakwashe", 
+                Surname = "Mhuriro", 
+                EmailAddress = "u20548312@tuks.co.za", 
+                myLink = "~/HTML/Kudakwashe.html" 
+            });
+            students.Add(new StudyGroupModel { StudentNo = 18242937, 
+                Name = "Hector", 
+                Surname = "Nhlabathi", 
+                EmailAddress = "u18242937@tuks.co.za", 
+                myLink = "~/HTML/Hector.html" 
+            });
+            students.Add(new StudyGroupModel { StudentNo = 19227206, 
+                Name = "Raphael", 
+                Surname = "Alphonso", 
+                EmailAddress = "u19227206@tuks.co.za", 
+                myLink = "~/HTML/Raphael.html" 
+            });
+            students.Add(new StudyGroupModel { StudentNo = 20509482, 
+                Name = "Claudius", 
+                Surname = "Phalane", 
+                EmailAddress = "u20509482@tuks.co.za", 
+                myLink = "~/HTML/Claudius.html" 
+            });
+            students.Add(new StudyGroupModel { StudentNo = 20554232, 
+                Name = "Kgaugelo", 
+                Surname = "Mosetlha", 
+                EmailAddress = "u20554232@tuks.co.za", 
+                myLink = "~/HTML/Kgaugelo.html" 
+            });
             return View(students);
         }
     }
